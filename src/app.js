5d849eec747b2e46ce02461403ab1d12c8265145
Arya <your_email@example.com>
2026-04-16 02:00:52 +0530

    fix static public folder issue

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ function createApp(options = {}) {
   });
 
   app.use(express.json({ limit: "1mb" }));
-  app.use(express.static(path.resolve(__dirname, "../public")));
+ // app.use(express.static(path.resolve(__dirname, "../public")));
 
   app.get("/api/health", (_request, response) => {
     response.json({
